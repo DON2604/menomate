dc6dff09451c0e743bdc166989fd65c26e96a371
Mrinmay Das <mrinmaydas2604@gmail.com>
2024-08-16 19:31:13 +0530

    feat: added welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:menomate/screens/home.dart';
+import 'package:menomate/screens/welcome.dart';
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body:  Home(),
+        body:  WelcomeScreen(),
       ),
     ),
   );
