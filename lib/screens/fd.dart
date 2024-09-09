cd3c04d2c688f8fd9e1708617d96be58d129f084
Mrinmay Das <mrinmaydas2604@gmail.com>
2024-09-09 20:37:13 +0530

    bug fix

--- lib/screens/fd.dart
+++ lib/screens/fd.dart
@@ -16,103 +16,85 @@ class Statistics extends StatelessWidget {
     Chart_Data('Dec', 16),
   ];
 
-  
   Statistics({super.key});
 
   @override
   Widget build(BuildContext context) {
     final List<PieData> piedata = <PieData>[
-    PieData("Happy", 45.2, Colors.blue),
-    PieData("Sad", 58.2, Colors.pink)
-  ];
+      PieData("Happy", 45.2, Colors.blue),
+      PieData("Sad", 58.2, Colors.pink)
+    ];
 
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment:
-              CrossAxisAlignment.start, // Align to start from left
-          children: [
-            const SizedBox(height: 40),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: 16.0), // Add padding for a modern look
-              child: SizedBox(
-                height:
-                    240, // Increased height to accommodate the title and chart
+        child: Padding(
+          padding: const EdgeInsets.all(16.0), // Add padding for a modern look
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 40),
+              const Text(
+                "Menstrual Cycle Tracker",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 8),
+              SizedBox(
+                height: 240, 
                 width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment
-                      .start, // Aligns text and chart to the left
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(
-                          bottom: 8.0), // Space between text and chart
-                      child: Text(
-                        "Menstrual Cycle Tracker",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
+                child: SfCartesianChart(
+                  plotAreaBorderWidth: 0,
+                  primaryXAxis: const CategoryAxis(
+                    majorGridLines: MajorGridLines(width: 0),
+                    edgeLabelPlacement: EdgeLabelPlacement.shift,
+                    axisLine: AxisLine(width: 0.5),
+                  ),
+                  primaryYAxis: const NumericAxis(
+                    majorGridLines: MajorGridLines(width: 0),
+                    axisLine: AxisLine(width: 0.5),
+                  ),
+                  series: <CartesianSeries>[
+                    SplineSeries<Chart_Data, String>(
+                      dataSource: data,
+                      xValueMapper: (Chart_Data data, _) => data.x,
+                      yValueMapper: (Chart_Data data, _) => data.y1,
+                      color: Colors.pinkAccent,
+                      width: 4,
+                      markerSettings: const MarkerSettings(
+                        isVisible: true,
+                        shape: DataMarkerType.circle,
+                        color: Colors.white,
+                        borderWidth: 2,
+                        borderColor: Colors.redAccent,
                       ),
                     ),
-                    Expanded(
-                      child: SfCartesianChart(
-                        plotAreaBorderWidth: 0,
-                        primaryXAxis: const CategoryAxis(
-                          majorGridLines: MajorGridLines(width: 0),
-                          edgeLabelPlacement: EdgeLabelPlacement.shift,
-                          axisLine: AxisLine(width: 0.5),
-                        ),
-                        primaryYAxis: const NumericAxis(
-                          majorGridLines: MajorGridLines(width: 0),
-                          axisLine: AxisLine(width: 0.5),
-                        ),
-                        series: <CartesianSeries>[
-                          SplineSeries<Chart_Data, String>(
-                            dataSource: data,
-                            xValueMapper: (Chart_Data data, _) => data.x,
-                            yValueMapper: (Chart_Data data, _) => data.y1,
-                            color: Colors.pinkAccent,
-                            width: 4,
-                            markerSettings: const MarkerSettings(
-                              isVisible: true,
-                              shape: DataMarkerType.circle,
-                              color: Colors.white,
-                              borderWidth: 2,
-                              borderColor: Colors.redAccent,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const Padding(
-                        padding: EdgeInsets.only(
-                            bottom: 8.0), // Space between text and chart
-
-                        child: SfCircularChart(
-                            series: <CircularSeries<PieData, String>>[
-                              RadialBarSeries<PieData, String>(
-                                  maximumValue: 6000,
-                                  radius: '100%',
-                                  gap: '3%',
-                                  dataSource: piedata,
-                                  cornerStyle: CornerStyle.bothCurve,
-                                  xValueMapper: (PieData piedata, _) =>
-                                      piedata.mood,
-                                  yValueMapper: (PieData piedata, _) =>
-                                      piedata.range,
-                                  pointColorMapper: (PieData data, _) =>
-                                      piedata.color)
-                            ]
-                        )
-                      )
                   ],
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 16), 
+              SizedBox(
+                height: 300, 
+                child: SfCircularChart(
+                  series: <CircularSeries<PieData, String>>[
+                    RadialBarSeries<PieData, String>(
+                      maximumValue: 100, 
+                      radius: '100%',
+                      gap: '3%',
+                      dataSource: piedata,
+                      cornerStyle: CornerStyle.bothCurve,
+                      xValueMapper: (PieData piedata, _) => piedata.mood,
+                      yValueMapper: (PieData piedata, _) => piedata.range,
+                      pointColorMapper: (PieData data, _) => data.color,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -125,10 +107,10 @@ class Chart_Data {
 
   Chart_Data(this.x, this.y1);
 }
+
 class PieData {
   final String mood;
   final double range;
-  MaterialColor color;
+  final Color color; 
   PieData(this.mood, this.range, this.color);
 }
-
